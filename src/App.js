66ff19b99931794d74128b0df6263d94de5477f2
Adarsh Ram <adarshram333@gmail.com>
2020-11-18 19:44:41 +0530

    Routing done and Navbar created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Navbar></Navbar>
+            <h2>Login ROute</h2>
+          </Route>
+          <Route path="/checkout">
+            <Navbar></Navbar>
+            <h2>Checkout ROute</h2>
+          </Route>
+          <Route path="/">
+            <Navbar></Navbar>
+            <h2>Home Page</h2>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+    return (
+        <div className="navbar">
+            {/* Logo */}
+               <h3 className="navbar__logo">eStore</h3> 
+            {/*Search Bar */}
+
+            {/* Buttons */}
+
+            {/* Basket */}
+        </div>
+    )
+}
+
+export default Navbar
